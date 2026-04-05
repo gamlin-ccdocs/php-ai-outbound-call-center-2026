e94397e818d9c6c5adf256971d18d7f056dc5dcc
Jason Shouldice <jason@ccdocs.com>
2026-04-04 20:15:46 -0500

    Initial commit: How to Build an AI-Powered Outbound Call Center From Scratch in 2026

---
+++ src/AiOutboundCallCenter2026.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * How to Build an AI-Powered Outbound Call Center From Scratch in 2026
+ *
+ * @see https://vicistack.com/blog/ai-outbound-call-center-2026/
+ * @package vicistack/ai-outbound-call-center-2026
+ */
+namespace ViciStack\AiOutboundCallCenter2026;
+
+class AiOutboundCallCenter2026
+{
+    /**
+     * @return string Documentation URL
+     */
+    public static function getDocumentationUrl(): string
+    {
+        return 'https://vicistack.com/blog/ai-outbound-call-center-2026/';
+    }
+
+    /**
+     * @return string Package topic
+     */
+    public static function getTopic(): string
+    {
+        return 'How to Build an AI-Powered Outbound Call Center From Scratch in 2026';
+    }
+}
